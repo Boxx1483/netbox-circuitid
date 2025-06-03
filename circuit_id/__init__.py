6647eaae7bf3a15131689b024712a9c5330699d3
Bo Mikkelsen [Netic] <bom@netic.dk>
2025-06-03 14:03:07 +0200

    Add config

--- circuit_id/__init__.py
+++ circuit_id/__init__.py
@@ -1,1 +1,13 @@
-from .netbox_plugin import config
+from netbox.plugins import PluginConfig
+
+
+class CircuitIDConfig(PluginConfig):
+    name = 'circuit_id'
+    verbose_name = 'Circuit ID'
+    description = 'Manage Circuit IDs for cables'
+    version = '0.1'
+    base_url = 'circuit-id'
+    required_settings = []
+    default_settings = {}
+
+config = CircuitIDConfig
